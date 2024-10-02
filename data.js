5cbb24a37fdeb2fccd538daf86328e7d5c396df9
Kimberly McCartney <94509134+kimccartney@users.noreply.github.com>
2024-10-01 22:58:43 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,83 @@
+[
+    {
+        "nombre": "Lampara",
+        "descripcion": "Lorem ipsum",
+        "fecha": "2022",
+        "signo": "Industrial",
+        "foto": "https://github.com/user-attachments/assets/9f9f997d-0255-4cf0-9839-b44cabb88ec8",
+        "github": "https://github.com/kimccartney"
+    },
+    {
+        "nombre": "Museo Taller",
+        "descripcion": "Lorem ipsum",
+        "fecha": "2022",
+        "signo": "Editorial",
+        "foto": "https://github.com/user-attachments/assets/0799411d-2aef-4457-8444-443e03655980",
+        "github": "https://github.com/kimccartney"
+    },
+    {
+        "nombre": "Libro Flexible",
+        "descripcion": "Lorem ipsum",
+        "fecha": "2024",
+        "signo": "Editorial",
+        "foto": "https://github.com/user-attachments/assets/07d64b2f-ea2f-4b67-a0d3-603719ce8de5",
+        "github": "https://github.com/kimccartney"
+    },
+    {
+        "nombre": "Infografia Taylor Swift",
+        "descripcion": "Lorem ipsum",
+        "fecha": "2024",
+        "signo": "Editorial",
+        "foto": "https://github.com/user-attachments/assets/1484c43d-da41-4d9d-8c76-59a1636164b0",
+        "github": "https://github.com/kimccartney"
+    },
+    {
+        "nombre": "FIBE",
+        "descripcion": "Lorem ipsum",
+        "fecha": "2023",
+        "signo": "Servicios",
+        "foto": "https://github.com/user-attachments/assets/26f61180-e965-441d-a946-2495e700a413",
+        "github": "https://github.com/kimccartney"
+    },
+    {
+        "nombre": "AFINA",
+        "descripcion": "Lorem ipsum",
+        "fecha": "2023",
+        "signo": "Servicios",
+        "foto": "https://github.com/user-attachments/assets/ac87abe8-0f9e-4514-8645-aee9dc4b1bc2",
+        "github": "https://github.com/kimccartney"
+    },
+    {
+        "nombre": "Toolkit Registro Civil",
+        "descripcion": "Lorem ipsum",
+        "fecha": "2024",
+        "signo": "Servicios",
+        "foto": "https://github.com/user-attachments/assets/f039d845-1503-492f-b293-8379b602d81b",
+        "github": "https://github.com/kimccartney"
+    },
+    {
+        "nombre": "Tinta Arte Impreso",
+        "descripcion": "Lorem ipsum",
+        "fecha": "2022",
+        "signo": "Editorial",
+        "foto": "https://github.com/user-attachments/assets/6daa78f5-7546-485d-b812-30b60619a68c",
+        "github": "https://github.com/kimccartney"
+    },
+    {
+        "nombre": "Taller TIDE",
+        "descripcion": "Lorem ipsum",
+        "fecha": "2022",
+        "signo": "Servicios",
+        "foto": "https://github.com/user-attachments/assets/10a2a2d4-1046-4537-92e8-c969b126ec69",
+        "github": "https://github.com/kimccartney"
+    },
+    {
+        "nombre": "Banca",
+        "descripcion": "Lorem ipsum",
+        "fecha": "2022",
+        "signo": "Industrial",
+        "foto": "https://github.com/user-attachments/assets/76d76d88-7fab-4454-ba4f-691a3dffa0fa",
+        "github": "https://github.com/kimccartney"
+    }
+]
+
